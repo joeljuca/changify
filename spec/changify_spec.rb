eb4d027d3ae8832b1c5e8a9d2bc54ef4e2753332
Joel Jucá <joelwallis@gmail.com>
2023-10-20 18:44:34 -0300

    test: add unit tests

--- spec/changify_spec.rb
+++ spec/changify_spec.rb
@@ -1,11 +1,61 @@
 # frozen_string_literal: true
 
+require_relative "../lib/changify"
+
 RSpec.describe Changify do
   it "has a version number" do
     expect(Changify::VERSION).not_to be nil
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Changify::Changedef do
+  it "expects a type" do
+    cd = Changify::Changedef.new("my_changedef", { "foo" => "bar" })
+
+    expect(cd.type).to eq("my_changedef")
+  end
+
+  it "expects some args" do
+    cd = Changify::Changedef.new("my_changedef", { "foo" => "bar" })
+
+    expect(cd.args).to be_an_instance_of(Hash)
+    expect(cd.args["foo"]).to eq("bar")
+  end
+
+  context ".from_jsonl" do
+    it "creates a Changedef from a JSONL" do
+      line = '["my_changedef", {"do":"something"}]'
+
+      cd = Changify::Changedef.from_jsonl(line)
+
+      expect(cd).to be_an_instance_of(Changify::Changedef)
+      expect(cd.type).to eq("my_changedef")
+      expect(cd.args).to be_an_instance_of(Hash)
+      expect(cd.args["do"]).to eq("something")
+    end
+  end
+end
+
+RSpec.describe Changify::Transform do
+  before do
+    @transform = Changify::Transform.new
+  end
+
+  context "#call(data, changedefs)" do
+    before do
+      step_set_name = lambda { |data, changedef|
+        data["name"] = changedef.args["name"] unless changedef.args["name"].nil?
+        data
+      }
+      @transform.add_step(step_set_name, only: "set.name")
+    end
+
+    it "transforms `data` using `changedefs`" do
+      changedef = Changify::Changedef.new("set.name", { "name" => "Joel" })
+
+      data = @transform.call({}, [changedef])
+
+      expect(data["name"]).to eq("Joel")
+    end
   end
 end
